11eda119755738833211fcd37246f60c9759219e
Luis Guilherme Polippo <luispolippo@hotmail.com>
2021-04-09 12:26:20 -0300

    main classes implemented

--- app/src/main/java/com/polippo/dessertpusher/MainActivity.kt
+++ app/src/main/java/com/polippo/dessertpusher/MainActivity.kt
@@ -1,11 +1,201 @@
 package com.polippo.dessertpusher
 
+import android.content.ActivityNotFoundException
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.core.app.ShareCompat
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.LifecycleObserver
+import com.polippo.dessertpusher.databinding.ActivityMainBinding
+import timber.log.Timber
 
-class MainActivity : AppCompatActivity() {
+const val KEY_REVENUE = "revenue_key"
+const val KEY_DESSERT_SOLD = "dessert_sold_key"
+const val KEY_TIMER_SECONDS = "timer_seconds_key"
+
+class MainActivity : AppCompatActivity(), LifecycleObserver {
+
+    private var revenue = 0
+    private var dessertsSold = 0
+    lateinit var dessertTimer: DessertTimer
+
+    //Contains All the Views
+    lateinit var binding: ActivityMainBinding
+
+    /** Dessert Data **/
+
+    /**
+     * Simple data class that represents a dessert. Includes the resource id integer associated with
+     * the image, the price it's sold for, and the startProductionAmount, which determines when
+     * the dessert starts to be produced.
+     */
+
+    data class Dessert(val imageId: Int, val price: Int, val startProductAmount: Int)
+
+    //create a list of all desserts, in order of when they start being produced
+    private val allDesserts = listOf(
+        Dessert(R.drawable.cupcake, 5, 0),
+        Dessert(R.drawable.donut, 10, 5),
+        Dessert(R.drawable.eclair, 15, 20),
+        Dessert(R.drawable.froyo, 30, 50),
+        Dessert(R.drawable.gingerbread, 50, 100),
+        Dessert(R.drawable.honeycomb, 100, 200),
+        Dessert(R.drawable.icecreamsandwich, 500, 500),
+        Dessert(R.drawable.jellybean, 1000, 1000),
+        Dessert(R.drawable.kitkat, 2000, 2000),
+        Dessert(R.drawable.lollipop, 3000, 4000),
+        Dessert(R.drawable.marshmallow, 4000, 8000),
+        Dessert(R.drawable.nougat, 5000, 16000),
+        Dessert(R.drawable.oreo, 6000, 20000)
+    )
+
+    private var currentDessert = allDesserts[0]
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        Timber.i("onCreate Called")
+
+        //Use Data Binding to reference the views
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.dessertButton.setOnClickListener {
+            onDessertClicked()
+        }
+
+        //Setup the DessertTimer, passing in the lifecycle
+        dessertTimer = DessertTimer(this.lifecycle)
+
+        //If there is a savedInstance bundle, then you're restarting activity
+        //If there isn't a bundle, then it's a fresh start
+        if(savedInstanceState != null){
+            //Get all the game state information from the bundle, set it
+            revenue = savedInstanceState.getInt(KEY_REVENUE, 0)
+            dessertsSold = savedInstanceState.getInt(KEY_DESSERT_SOLD, 0)
+            dessertTimer.secondsCount = savedInstanceState.getInt(KEY_TIMER_SECONDS, 0)
+            showCurrentDessert()
+        }
+
+        //Set the TextView to the right values
+        binding.revenue = revenue
+        binding.amountSold = dessertsSold
+
+        //Make sure the correct dessert is showing
+        binding.dessertButton.setImageResource(currentDessert.imageId)
+    }
+    /*
+    *Determine which dessert to show
+    */
+    private fun showCurrentDessert() {
+        var newDessert = allDesserts[0]
+        for(dessert in allDesserts){
+            if(dessertsSold >= dessert.startProductAmount){
+                newDessert = dessert
+            }
+            //The list of desserts is sorted by startProductionAmount. As you sell more desserts,
+            //you'll start producing more expensive desserts as determined by startProductionAmount
+            //We know to break as soon as we see a dessert who's "startProductionAmount" is greater
+            //then the amount sold.
+            else break
+        }
+
+        //If the new dessert is actually different than the current dessert, update the image
+        if(newDessert != currentDessert){
+            currentDessert = newDessert
+            binding.dessertButton.setImageResource(newDessert.imageId)
+        }
+    }
+
+    /*
+    *Update score when the dessert is clicked. Possibly shows a new dessert
+    */
+    private fun onDessertClicked() {
+        //Update the score
+        revenue += currentDessert.price
+        dessertsSold++
+
+        binding.revenue = revenue
+        binding.amountSold = dessertsSold
+
+        //show the next dessert
+        showCurrentDessert()
+    }
+
+    /**
+     * Menu methods
+     */
+    private fun onShare(){
+        val shareIntent = ShareCompat.IntentBuilder.from(this)
+            .setText(getString(R.string.share_text, dessertsSold, revenue))
+            .setType("text/plain")
+            .intent
+
+        try{
+            startActivity(shareIntent)
+        } catch (ex: ActivityNotFoundException){
+            Toast.makeText(this, getString(R.string.sharing_not_available), Toast.LENGTH_LONG).show()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.shareMenuButton -> onShare()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    /**
+     * Called when the users navigate away from the app but might come back
+     */
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+        outState.putInt(KEY_REVENUE, revenue)
+        outState.putInt(KEY_DESSERT_SOLD, dessertsSold)
+        outState.putInt(KEY_TIMER_SECONDS, dessertTimer.secondsCount)
+        Timber.i("onSavedInstanceState Called")
+        super.onSaveInstanceState(outState, outPersistentState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        Timber.i("onRestoreInstanceState Called")
+    }
+
+    /** LifeCycle Methods **/
+    override fun onStart() {
+        super.onStart()
+        Timber.i("onStart Called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Timber.i("onResume Called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Timber.i("onPause Called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Timber.i("onStop Called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Timber.i("onDestroy Called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Timber.i("onRestart Called")
     }
 }
---
+++ app/src/main/java/com/polippo/dessertpusher/PusherApplication.kt
@@ -0,0 +1,11 @@
+package com.polippo.dessertpusher
+
+import android.app.Application
+import timber.log.Timber
+
+class PusherApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
+}
